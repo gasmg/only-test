001b198e640d97b35b44e271b2dfa91ab6e9d6c0
gasmg <kprogrammistov@yandex.ru>
2022-07-07 08:26:28 +0300

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Normalize } from "styled-normalize";
+import App from "./App";
+
+const root = ReactDOM.createRoot(
+	document.getElementById("root") as HTMLElement
+);
+
+root.render(
+	<React.StrictMode>
+		<Normalize />
+		<App />
+	</React.StrictMode>
+);
